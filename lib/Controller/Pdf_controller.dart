ef7dd5b93f866c08e4c996d9fde853df4014e5fb
sainiaman20 <sainiaman3923@gmail.com>
2024-06-07 22:50:50 +0530

    Fifth Commit: Completed Read Book Page

---
+++ lib/Controller/Pdf_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class Pdf_controller extends GetxController {
+  final GlobalKey<SfPdfViewerState> pdfViewerKey = GlobalKey();
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
